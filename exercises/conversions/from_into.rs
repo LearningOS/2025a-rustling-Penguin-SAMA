d71f8c5548a720a065ec95f50cb95df9cc36101c
Penguin-SAMA <496107146@qq.com>
2025-09-21 15:59:09 +0800

    update

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,27 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.is_empty() {
+            return Person::default();
+        }
+
+        let mut parts = s.split(",");
+        let name = parts.next().unwrap_or("");
+        let age = parts.next().unwrap_or("");
+
+        if name.is_empty() || parts.next().is_some() {
+            return Person::default();
+        }
+
+        match age.parse::<usize>() {
+            Ok(age) => Person {
+                name: name.to_string(),
+                age,
+            },
+            Err(_) => Person::default(),
+        }
     }
 }
 
